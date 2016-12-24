9db89ed187778ee34c94bb33d8ef73a5478df1e6
Matias Lopez <imatlopez@gmail.com>
2016-12-24 17:33:48 -0500

    TEMP Debug

--- const.js
+++ const.js
@@ -11,6 +11,7 @@ const FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN;
 
 var FB_VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN;
 if (!FB_VERIFY_TOKEN) {
+  Console.log("Replacing by WIT_TOKEN: " + WIT_TOKEN);
   FB_VERIFY_TOKEN = WIT_TOKEN;
 }
 
