36f0563d3c86338d95aa8521ac603b07b78ec0c1
Matias Lopez <imatlopez@gmail.com>
2016-12-24 00:34:32 -0500

    Kostis update

--- server.py
+++ server.py
@@ -1,42 +1,59 @@
-# coding: utf-8
-
-import warnings
-import os
 from flask import Flask, request
-
-import chatbot
-import messenger
+import json
+import requests
 
 app = Flask(__name__)
 
-FACEBOOK_TOKEN = os.environ['EAARMx0PABlkBAGXrLLIZAspkYEBrZBI2GaBVOE0Fetqhpf1X7jZCTGxcZBqzRGdDarrLOEhZApUy0cLAkCEqYfRUXkT70pFTGtTgrQhmlAHYEZB1w7pAZCjfvKwqlTdZB6s1KZBOqjRzZBkcTCZBGyT9bPva9xyoCAYwYJfS599hQT7mAZDZD']
-bot = None
+# This needs to be filled with the Page Access Token that will be provided
+# by the Facebook App that will be created.
+PAT = 'EAARMx0PABlkBAGXrLLIZAspkYEBrZBI2GaBVOE0Fetqhpf1X7jZCTGxcZBqzRGdDarrLOEhZApUy0cLAkCEqYfRUXkT70pFTGtTgrQhmlAHYEZB1w7pAZCjfvKwqlTdZB6s1KZBOqjRzZBkcTCZBGyT9bPva9xyoCAYwYJfS599hQT7mAZDZD'
 
 @app.route('/', methods=['GET'])
-def verify():
-    if request.args.get('hub.verify_token', '') == os.environ['hi_my_name_is_marvis_the_jello']:
-        return request.args.get('hub.challenge', '')
-    else:
-        return 'Error, wrong validation token'
+def handle_verification():
+  print "Handling Verification."
+  if request.args.get('hub.verify_token', '') == 'my_voice_is_my_password_verify_me':
+    print "Verification successful!"
+    return request.args.get('hub.challenge', '')
+  else:
+    print "Verification failed!"
+    return 'Error, wrong validation token'
 
 @app.route('/', methods=['POST'])
-def webhook():
-    payload = request.get_data()
-    for sender, message in messenger.messaging_events(payload):
-        print "Incoming from %s: %s" % (sender, message)
+def handle_messages():
+  print "Handling Messages"
+  payload = request.get_data()
+  print payload
+  for sender, message in messaging_events(payload):
+    print "Incoming from %s: %s" % (sender, message)
+    send_message(PAT, sender, message)
+  return "ok"
 
-        response = bot.respond_to(message)
+def messaging_events(payload):
+  """Generate tuples of (sender_id, message_text) from the
+  provided payload.
+  """
+  data = json.loads(payload)
+  messaging_events = data["entry"][0]["messaging"]
+  for event in messaging_events:
+    if "message" in event and "text" in event["message"]:
+      yield event["sender"]["id"], event["message"]["text"].encode('unicode_escape')
+    else:
+      yield event["sender"]["id"], "I can't echo this"
 
-        print "Outgoing to %s: %s" % (sender, response)
-        messenger.send_message(FACEBOOK_TOKEN, sender, response)
 
-    return "ok"
+def send_message(token, recipient, text):
+  """Send the message text to recipient with id recipient.
+  """
+
+  r = requests.post("https://graph.facebook.com/v2.6/me/messages",
+    params={"access_token": token},
+    data=json.dumps({
+      "recipient": {"id": recipient},
+      "message": {"text": text.decode('unicode_escape')}
+    }),
+    headers={'Content-type': 'application/json'})
+  if r.status_code != requests.codes.ok:
+    print r.text
 
 if __name__ == '__main__':
-    # Suppress nltk warnings about not enough data
-    warnings.filterwarnings('ignore', '.*returning an arbitrary sample.*',)
-
-    if os.path.exists("corpus.txt"):
-        bot = chatbot.Bot(open("corpus.txt").read())
-
-    app.run(port=3000, debug=True)
+  app.run()
